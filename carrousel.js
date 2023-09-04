74d1a789c7881a6d0da107b77fe3065bf1a72ab7
danielVillalb <villalbacarlosd97@gmail.com>
2023-09-03 22:18:01 -0300

    carrusel sin flechas

--- carrousel.js
+++ carrousel.js
@@ -2,8 +2,8 @@
 $('.galeria').slick({
     dots: true,
     infinite: true,
-
-    pauseOnFocus:true,
+    pauseOnHover:true,
+    arrows:false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
@@ -14,8 +14,8 @@ $('.galeria').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
